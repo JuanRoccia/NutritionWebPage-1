fbdab66b41e7d46135a1ec07a41ed550aae7c3d1
Juan Manuel Roccia <106710715+JuanRoccia@users.noreply.github.com>
2023-01-25 18:01:48 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,27 @@
+/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("header").style.top = "0";
+  } else {
+    document.getElementById("header").style.top = "-70px";
+  }
+  prevScrollpos = currentScrollPos;
+}
+
+// Automatic Slideshow
+var slideIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > x.length) { slideIndex = 1 }
+  x[slideIndex - 1].style.display = "block";
+  setTimeout(carousel, 5000); // Change image every 2 seconds
+}
